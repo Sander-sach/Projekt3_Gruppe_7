eaa56ee72b2564bc0280601d856cd465ad271a30
Sander <sander.o.christensen@gmail.com>
2026-05-07 14:00:02 +0200

    Initial Spring Boot project setup

---
+++ src/main/java/com/example/projekt3_gruppe_7/Projekt3Gruppe7Application.java
@@ -0,0 +1,13 @@
+package com.example.projekt3_gruppe_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt3Gruppe7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt3Gruppe7Application.class, args);
+    }
+
+}
