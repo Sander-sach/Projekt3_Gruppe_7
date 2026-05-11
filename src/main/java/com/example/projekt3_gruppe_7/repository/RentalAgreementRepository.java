f65749169aa6bbe5ec1421bd6b89f4af99ff8ef8
Sander <sander.o.christensen@gmail.com>
2026-05-12 00:15:47 +0200

    Oprettet Repository struktur til interface med JDBC

--- src/main/java/com/example/projekt3_gruppe_7/repository/RentalAgreementRepository.java
+++ src/main/java/com/example/projekt3_gruppe_7/repository/RentalAgreementRepository.java
@@ -1,4 +1,39 @@
 package com.example.projekt3_gruppe_7.repository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import com.example.projekt3_gruppe_7.model.RentalAgreement;
 
-public class RentalAgreementRepository {
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RentalAgreementRepository implements BaseRepository<RentalAgreement> {
+
+    @Autowired
+    private DataSource dataSource;
+
+    public RentalAgreement findById(Long id){
+        RentalAgreement rentalAgreement =null;
+
+        return rentalAgreement;
+    }
+
+    public List<RentalAgreement> findAll(){
+        List<RentalAgreement> list = new ArrayList<>();
+
+        return list;
+    }
+
+    public void save(RentalAgreement entity){
+
+    }
+
+    public void update(RentalAgreement entity){
+
+    }
+
+    public void delete(Long id){
+
+    }
 }
