419ef0d7d995b36a24325e576ddcf898641f67e2
valdemarjacobsen <vaja0001@stud.ek.dk>
2026-05-19 16:09:49 +0200

    CarController samt CarService

--- src/main/java/com/example/projekt3_gruppe_7/controller/CarController.java
+++ src/main/java/com/example/projekt3_gruppe_7/controller/CarController.java
@@ -1,7 +1,43 @@
 package com.example.projekt3_gruppe_7.controller;
 
+import com.example.projekt3_gruppe_7.service.CarService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class CarController {
+
+    private final CarService carService;
+
+    @Autowired
+    public CarController (CarService carService) {
+        this.carService = carService;
+    }
+    @GetMapping("/cars/storage")
+    public String carStorage(Model model){
+        model.addAttribute("rented",carService.getRented());
+        model.addAttribute("available",carService.getAvaliable());
+        model.addAttribute("returned",carService.getReturned());
+        model.addAttribute("awaitingRegistration", carService.getAwaitingRegistration());
+        model.addAttribute("underRepair",carService.getUnderRepair());
+        return "cars-storage";
+    }
+    @GetMapping("/cars/rented")
+    public String carsRented(Model model){
+        model.addAttribute("rented",carService.getRented());
+        return "cars-rented";
+    }
+    @GetMapping("/cars/overview")
+    public String carsOverview(Model model){
+        model.addAttribute("rented",carService.getRented());
+        model.addAttribute("available",carService.getAvaliable());
+        model.addAttribute("returned",carService.getReturned());
+        model.addAttribute("awaitingRegistration", carService.getAwaitingRegistration());
+        model.addAttribute("underRepair",carService.getUnderRepair());
+        return "cars-overview";
+    }
+
+
 }
--- src/main/java/com/example/projekt3_gruppe_7/service/CarService.java
+++ src/main/java/com/example/projekt3_gruppe_7/service/CarService.java
@@ -1,9 +1,13 @@
 package com.example.projekt3_gruppe_7.service;
 
 import com.example.projekt3_gruppe_7.model.Car;
+import com.example.projekt3_gruppe_7.model.CarStatus;
 import com.example.projekt3_gruppe_7.repository.CarRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CarService {
@@ -18,4 +22,33 @@ public class CarService {
     public Car findCarById(Long carId){
         return carRepositoryImpl.findById(carId);
     }
+
+    public List<Car> getCarByStatus(CarStatus status){
+        List<Car> allCars = carRepositoryImpl.findAll();
+        List<Car> result = new ArrayList<Car>();
+
+        for (Car car : allCars){
+            if (car.getStatus()== status){
+                result.add(car);
+            }
+        }
+        return result;
+
+    }
+    public List<Car> getRented(){
+        return getCarByStatus(CarStatus.RENTED);
+    }
+    public List<Car> getAvaliable(){
+        return getCarByStatus(CarStatus.AVAILABLE);
+    }
+    public List<Car> getReturned(){
+        return getCarByStatus(CarStatus.RETURNED);
+    }
+    public List<Car> getAwaitingRegistration(){
+        return getCarByStatus(CarStatus.AWAITING_REGISTRATION);
+    }
+    public List<Car> getUnderRepair(){
+        return getCarByStatus(CarStatus.UNDER_REPAIR);
+    }
+
 }
